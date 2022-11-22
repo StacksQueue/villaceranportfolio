28d57f2762d5aae1b50f05f395738cdf171adb61
stacksqueue <paulelyson.villaceran@gmail.com>
2022-11-22 23:00:50 +0800

    update modal display

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,12 @@
 var modal = document.getElementById("modal");
+var modalcontent = document.getElementsByClassName("modal-content")[0];
 var btnclose = document.getElementsByName("close")[0];
 var projects = document.getElementsByClassName("project-wrapper");
 
-Array.from(projects).forEach((project) => {
-  console.log(project);
-  project.addEventListener("click", () => {
+Array.from(projects).forEach((project, index) => {
+  let projectname = project.getAttribute("name");
+  project.addEventListener("click", (evt) => {
+    displayIframe(projectname)
     modal.style.display = "block";
     setTimeout(() => {
       modal.style.opacity = 1;
@@ -18,3 +20,12 @@ btnclose.onclick = () => {
     modal.style.display = "none";
   }, 500);
 };
+
+function displayIframe(projectname) {
+  var iframe= document.createElement('iframe');
+    iframe.src= './../project-vtuber.html';
+    iframe.width="100%"
+    iframe.height="100%"
+    iframe.frameBorder = 0;
+    modalcontent.appendChild(iframe);
+}
